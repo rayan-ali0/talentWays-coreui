cf7262b490b09600aa9b86da8e63d41fb2420b7e
Rayan ALi <rayan.ali.8205@gmail.com>
2025-08-19 10:32:01 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './tailwind.css'
+
+function App() {
+
+  return (
+    <>
+    <div>my routes</div>
+      {/* <Routes>
+        <Route path="/" element={<myPage />} />
+      </Routes> */}
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  future: {
+    hoverOnlyWhenSupported: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@aegov/design-system'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+
+  ],
+}
+
